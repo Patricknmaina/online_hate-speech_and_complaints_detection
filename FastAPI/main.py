64449ed461c4221f36615dfb5ae727dd9754bdd6
Patrick Maina <patrickmaina364@gmail.com>
2025-09-11 18:25:52 +0300

    added files for heroku deployment

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -361,5 +361,6 @@ async def get_model_info():
 
 # -------------------------- Main --------------------------
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import uvicorn, os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
